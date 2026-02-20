a321e792c8d907208e9442c4dca0111ebe50af5b
jay jay <72658288+Arjay-lab@users.noreply.github.com>
2026-02-20 12:29:51 +0800

    Staatic deployment 5fa934f62fc7

--- wp-content/plugins/Newsmag/includes/demos/food/td_media_6.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by ra on 6/13/2015.
- */
-
-
-//ads
-td_demo_media::add_image_to_media_gallery('td_food_ad_sidebar',       "http://demo_content.tagdiv.com/Newsmag/food/rec-sidebar.jpg");
